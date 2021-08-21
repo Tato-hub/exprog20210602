78f3c837f10596ea7a1b3e6e9867563770c772c2
Cesar <60903324+Tato-hub@users.noreply.github.com>
2021-08-21 20:20:24 +0200

    upd

--- Examen/src/Ejercicio1.java
+++ Examen/src/Ejercicio1.java
@@ -4,6 +4,8 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo Aqui estoy yo");
+		System.out.println("Hola mundo Aqui estoy yo");
+		
 	}
 
 }
